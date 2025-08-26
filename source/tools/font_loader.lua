82931a8f4d2e7dd127e8354fe7095d3ed75df294
Nazar Polyvoda <nanskip.oof@gmail.com>
2025-08-26 17:36:08 +0200

    updated font atlas

--- source/tools/font_loader.lua
+++ source/tools/font_loader.lua
@@ -6,7 +6,7 @@ local mod = {}
 
 mod.createText = function(self, text, fontData, fontImage, config)
     local defaultConfig = {
-        color = Color(0, 0, 0),
+        color = Color(0, 0, 0, 255),
         size = 20,
         pos = Number2(0, 0),
     }
@@ -17,24 +17,28 @@ mod.createText = function(self, text, fontData, fontImage, config)
 
     group.config = config
     group.text = text
-    group.fontData = fontData
+    group.fontData = JSON:Decode(fontData)
     group.fontImage = fontImage
     group.frames = {}
 
     -- create frames
 
     group.currentOffsetX = 0
-    for ch in s:gmatch(group.text) do
+    for ch in group.text:gmatch('.') do
+        print(ch)
         local frame = _UI:createFrame()
         local data = group.fontData[ch]
 
         frame.size = Number2(data.w, data.h)
         frame.pos = Number2(
-            group.config.pos.X + data.x + group.currentOffsetX,
-            group.config.pos.Y + data.y
+            group.config.pos.X + group.currentOffsetX,
+            group.config.pos.Y
         )
         group.currentOffsetX = group.currentOffsetX + data.w
         frame.object.Image = { data = group.fontImage, alpha = true}
+        frame.object.Tiling = Number2(data.w/546, data.h/780)
+        frame.object.Offset = Number2(data.x/546, data.y/780)
+        frame.Color = group.config.color
     end
 
     group.update = function(_)
